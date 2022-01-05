00b18320bcf5ef6fe0bc5a1a01b3578abeedfe1b
Vladimir Matarrita <vmatarrita88@gmail.com>
2022-01-05 11:14:50 -0600

    Scripts were modified to run on development and production

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const checkAuth = require("../middleware/check-auth");
+const checkAuth = require("../middlewares/check-auth");
 const userController = require("../controllers/user");
 
 router.post("/signup", checkAuth, userController.user_signup);
